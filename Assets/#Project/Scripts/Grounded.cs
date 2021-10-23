27ebd0f91f182be48e5c3338f03dc3888ef27a78
A <urbaniak_agata@hotmail.com>
2021-10-23 23:00:34 +0200

    SecondCommit - isGrounded still doesn't work

---
+++ Assets/#Project/Scripts/Grounded.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+
+
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerController>().isGrounded = true;
+            Debug.Log("OnDaGround");
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerController>().isGrounded = false;
+            Debug.Log("Nope, I'm not OnDaGround");
+        }
+
+    }
+}
