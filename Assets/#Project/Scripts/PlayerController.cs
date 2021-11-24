565b29fe3200ef18cea0c485bd4e69efaf223f89
Agata <urbaniak_agata@hotmail.com>
2021-11-24 14:47:19 +0100

    ok gud

--- Assets/#Project/Scripts/PlayerController.cs
+++ Assets/#Project/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     protected PlayerControls playerControls;
     public float moveSpeed = 15f;
-    public float jumpForce= 15f;
+    public float jumpForce = 15f;
 
     public bool isGrounded;
     public bool canDoubleJump;
@@ -15,22 +15,32 @@ public class PlayerController : MonoBehaviour
     //private Rigidbody2D theRB;
 
     private SpriteRenderer spriteRenderer;
-    public bool flipX;
+
 
 
     //public Transform GroundCheck;
     //public Transform CeilingCheck;
-    [Tooltip("Monster speed")]
+    [Tooltip("speed")]
     public Vector2 speed2 = Vector2.zero;
 
 
     private float speed;
     private Vector3 move;
 
+
+    private float originalXScale;
+    private int direction;
+
+
+
     private void Start()
     {
-        //theRB = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // scale relative to parent space
+        //transform.localScale = new Vector3(0.1f, 0.1f, 1f);
+        originalXScale = transform.localScale.x;
+
+        direction = 1;
     }
 
 
@@ -43,12 +53,17 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         playerControls.Enable();
-        
+
     }
     private void OnDisable()
     {
         playerControls.Disable();
 
+    }
+
+    private void FixedUpdate()
+    {
+        //float xSpeed = speed * playerControls.Player.Move;
     }
 
 
@@ -58,7 +73,8 @@ public class PlayerController : MonoBehaviour
         speed = moveSpeed * Time.deltaTime;
         Move();
 
-        if (isGrounded == true) { 
+        if (isGrounded == true)
+        {
             Jump();
             canDoubleJump = true;
             //Debug.Log(speed);
@@ -71,28 +87,35 @@ public class PlayerController : MonoBehaviour
             //Debug.Log(speed);
         }
 
-        //if (speed2.x < 0)
-        //{
-        //    spriteRenderer.flipX = true;
+        if (speed2.x < 0)
+        {
+            Debug.Log("Going Right");
+            //Flip();
 
-        //}
-        //else
-        //{
-        //    spriteRenderer.flipX = false;
 
-        //}
 
+        }
+        else
+        {
+            Debug.Log("Going Left");
+            //Flip();
+
+
+        }
 
 
 
 
 
     }
-    //protected void FixedUpdate()
-    //{
-        
-        
-    //}
+
+    private void Flip()
+    {
+
+        transform.localScale = new Vector3(-0.1f, 0.1f, 1f);
+
+
+    }
 
     void Jump()
     {
@@ -123,4 +146,26 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void CharacterDirectionCheck(float xSpeed)
+    {
+        // If the sign of the character's horizontal speed
+        // is different from the sign of the direction they are facing in,
+        // then we need to flip the character to face the other direction.
+        if (xSpeed * direction < 0.0f)
+        {
+            // Flip the direction the character is facing in
+            direction *= -1;
+
+            // Record the character's current scale
+            Vector3 scale = transform.localScale;
+
+            // Set the character's X-scale to be their original scale
+            // times the direction they are facing in
+            scale.x = originalXScale * direction;
+
+            // Apply the new scale to the character's transform
+            transform.localScale = scale;
+        }
+
+    }
 }
